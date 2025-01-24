68402511bd5f67e0fe6ac287f80467853f568a2f
MatkoCausic <matko.causic@gmail.com>
2025-01-24 15:28:58 +0100

    Added new pages

--- BazaarBuddy/bazaarbuddy/bazaarbuddy/urls.py
+++ BazaarBuddy/bazaarbuddy/bazaarbuddy/urls.py
@@ -4,6 +4,7 @@ from core import views
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('products/', views.index, name='index'),
+    path('', views.home, name='homepage'),
     path('admin/', admin.site.urls),
 ]
--- BazaarBuddy/bazaarbuddy/core/views.py
+++ BazaarBuddy/bazaarbuddy/core/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'core/index.html')
+
+def home(request):
+    return render(request, 'core/home.html')
