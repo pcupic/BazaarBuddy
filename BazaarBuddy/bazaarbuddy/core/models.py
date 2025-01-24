a0fa8d46ddcd2f84dc348d4fdf94279c8127920f
pcupic <cupicpatrik@gmail.com>
2025-01-24 12:08:35 +0100

    add role to models

--- BazaarBuddy/bazaarbuddy/core/models.py
+++ BazaarBuddy/bazaarbuddy/core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
-# Create your models here.
+
+class Role(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50, unique=True)
+    
+    
+
+    
