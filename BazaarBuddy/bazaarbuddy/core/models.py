b9f6687e8c280f00086b3e5f96aa0240acce16b9
pcupic <cupicpatrik@gmail.com>
2025-01-24 12:09:16 +0100

    add user model

--- BazaarBuddy/bazaarbuddy/core/models.py
+++ BazaarBuddy/bazaarbuddy/core/models.py
@@ -8,5 +8,10 @@ class Role(models.Model):
     name = models.CharField(max_length=50, unique=True)
     
     
-
+class User(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    roleId = models.ForeignKey(Role, on_delete=models.CASCADE) 
     
