24510dc1f75c7828a020f2d221b1d9963ae5b0ba
pcupic <cupicpatrik@gmail.com>
2025-01-24 12:55:39 +0100

    update models.py

--- BazaarBuddy/bazaarbuddy/core/models.py
+++ BazaarBuddy/bazaarbuddy/core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 import uuid
 from django.core.validators import MinValueValidator, MaxValueValidator
-from models import Rating, Rating
+from models import Rating, Category
 
 
 class Role(models.Model):
