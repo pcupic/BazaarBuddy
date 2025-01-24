56509921182a0b23a4e8b8e747279861746be8fa
pcupic <cupicpatrik@gmail.com>
2025-01-24 14:01:05 +0100

    test

--- BazaarBuddy/bazaarbuddy/core/models.py
+++ BazaarBuddy/bazaarbuddy/core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 import uuid
 from django.core.validators import MinValueValidator, MaxValueValidator
-from models import Rating, Category
+
     
 class User(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
