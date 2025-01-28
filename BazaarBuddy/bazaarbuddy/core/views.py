035474e61d2ae8ea35341f25cae2356a65476781
MatkoCausic <matko.causic@gmail.com>
2025-01-28 18:01:18 +0100

    Made filters, they don't work well

--- BazaarBuddy/bazaarbuddy/core/views.py
+++ BazaarBuddy/bazaarbuddy/core/views.py
@@ -3,12 +3,35 @@ from .forms import ProductForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
-from .models import Product
+from .models import Product,Category
 
 @login_required
 def index(request):
-    products = Product.objects.filter(state = Product.State.ACCEPTED)
-    return render(request, 'core/index.html', {'products': products})
+    products = Product.objects.all()
+    categories = Category.objects.all()
+
+    category_id = request.GET.get('category')
+    condition = request.GET.get('condition')
+    sort = request.GET.get('sort')
+
+    if category_id:
+        products = products.filter(category_id = category_id)
+    
+    if condition:
+        products = products.filter(condition = condition)
+    
+    if sort == "asc":
+        products = products.order_by("price")
+    elif sort == "desc":
+        products = products.order_by("-price")
+    
+    context = {
+        'products': products,
+        'categories': categories,
+    }
+    return render(request, 'core/index.html', context)
+    # products = Product.objects.filter(state = Product.State.ACCEPTED)
+    # return render(request, 'core/index.html', {'products': products})
 
 @login_required
 def home(request):
